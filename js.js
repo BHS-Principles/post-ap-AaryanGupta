e00e3473e8d320a1cdeeecdfb20e0ae98ec71a6d
Aaryan Gupta <122922835+AARYANGUPTA12@users.noreply.github.com>
2025-05-20 16:31:38 +0000

    sgima

---
+++ js.js
@@ -0,0 +1,11 @@
+var LIST [
+    "Cool boy"
+    "Wat da Sigma"
+    "WOw"
+];
+var BTN = document.getElementById("myButton");
+for ( var i = 0; i < 3 ; i++ ){
+    var newBTN = BTN.cloneNode(true);
+    document.body.append(newBtn);
+    alert (" Check sigma" + LIST [i]);
+}
